c435c07f74045b6ce3e97c3150a6fe4537f9fe45
Jonas T. Schmid <jonas.till.schmid@gmail.com>
2024-10-12 23:54:38 +0200

    Refactor plotting functions and add argument parsing.
    Separated the extraction and plotting logic into distinct functions for clarity. Enhanced the plotting function to optionally include a "Rest" category in pie charts. Added argparse for command-line argument parsing and removed unused openpyxl import.

--- parteifinanizerung.py
+++ parteifinanizerung.py
@@ -3,11 +3,17 @@ import os
 import re
 
 import numpy as np
-import openpyxl
 import pandas
 import seaborn as sns
 import matplotlib.pyplot as plt
 from difflib import SequenceMatcher
+import argparse
+COLORS = {
+    "SP": "red",
+    "": "yellow",
+    "FDP.Die Liberalen": "blue",
+    "Die Mitte": "orange"
+}
 
 COLNAME_AMOUNT = "Wert (in CHF)"
 
@@ -18,13 +24,8 @@ def rightstrip_with_wildcards(s, pattern):
     return regex.sub("", s)
 PARTY_NAME = "Akteur"
 NUMBEROFDONORSPERPLOT = 10
-MULTIPLEPLOTSINONE =True
 
-wb = openpyxl.load_workbook("./2023_Parteifinanzierung.xlsx")
-sheet = wb.sheetnames
-print(sheet)
-df = pandas.read_excel("./2023_Parteifinanzierung.xlsx", sheet_name="Zuwendungen")
-print(df)
+
 def renamesimilar(df: pandas.DataFrame, column: str, threshold: float = 0.9):
     droppedindexes = []
     for index, row in df.iterrows():
@@ -35,90 +36,136 @@ def renamesimilar(df: pandas.DataFrame, column: str, threshold: float = 0.9):
             if similarity > threshold:
                 df.at[index, column] = otherrow[column]
     return df
-for offenlegungslauf in set(df["Offenlegungslauf"]):
 
+
+def plotpartydonorsperparty(filelocation, showrest, MULTIPLOTSINONE=True):
+    global rowsdonor
+    df = get_excel_data(filelocation)
+    print(df)
+    for offenlegungslauf in set(df["Offenlegungslauf"]):
+
+        ldf = extract_year_and_relevantdata(df, offenlegungslauf)
+        parties = list(set(ldf[PARTY_NAME]))
+        numparties = len(parties)
+        fig, axes = plt.subplots(int(np.ceil(np.sqrt(numparties))), int(np.ceil(np.sqrt(numparties))), figsize=(20, 20))
+        plt.rcParams.update({'font.size': 14})
+
+        os.makedirs(f"plots/{offenlegungslauf.replace(' ', '')}", exist_ok=True)
+
+        for party in parties:
+            partydonors = ldf[ldf[PARTY_NAME] == party]
+            partydonors["Merged_Name"] = partydonors["Name des Urhebers der Zuwendung"].fillna('') + ' ' + partydonors[
+                "Vorname des Urhebers der Zuwendung"].fillna('') + ' ' + partydonors["Wohnsitzgemeinde"].fillna('')
+            partydonors["Merged"] = partydonors["Firma des Urhebers der Zuwendung"].fillna('') + ' ' + partydonors[
+                "Merged_Name"].str.strip()
+            partydonors["Merged"] = partydonors["Merged"].apply(lambda row: row.rstrip(" "))
+            partydonors["Merged"] = partydonors["Merged"].apply(lambda row: rightstrip_with_wildcards(row, "(*)"))
+            partydonors = renamesimilar(partydonors, "Merged")
+            a = partydonors.nlargest(NUMBEROFDONORSPERPLOT, "Wert (in CHF)")
+
+            if showrest and len(partydonors)> len(a):
+                a.loc[len(a)] = {"Merged": "Rest", COLNAME_AMOUNT: 0, "Akteur": party}
+
+                a[a["Merged"]=="Rest"][COLNAME_AMOUNT]= partydonors["Wert (in CHF)"].sum() - a["Wert (in CHF)"].sum()
+            if MULTIPLOTSINONE:
+                ax = axes.flatten()[parties.index(party)]
+                ax.pie(a["Wert (in CHF)"], labels=a["Merged"])
+                ax.set_title(f"Zuwendungen {party} {offenlegungslauf}")
+            else:
+                plt.pie(a["Wert (in CHF)"], labels=a["Merged"])
+                title = f"Zuwendungen {party} {offenlegungslauf}"
+                if NUMBEROFDONORSPERPLOT < np.inf:
+                    f"Zuwendungen {party} Top {NUMBEROFDONORSPERPLOT}"
+                plt.title(title)
+                # plt.show()
+                plt.tight_layout()
+                plt.savefig(f"plots/{offenlegungslauf.replace(' ', '')}/{party}.png")
+                plt.clf()
+        if MULTIPLOTSINONE:
+            for j in range(len(parties), len(axes.flatten())):
+                fig.delaxes(axes.flatten()[j])
+
+            fig.tight_layout(rect=[0, 0, 1, 0.95])
+            suptitle = f"Zuwendungen für {offenlegungslauf}"
+            if NUMBEROFDONORSPERPLOT < np.inf:
+                suptitle += f" Top (n={NUMBEROFDONORSPERPLOT})"
+            plt.suptitle(suptitle)
+            plt.savefig(f"plots/{offenlegungslauf.replace(' ', '')}/all.png")
+
+        plt.clf()
+        df = plotperdonor()
+
+
+def extract_year_and_relevantdata(df, offenlegungslauf):
     ldf = df[df["Offenlegungslauf"] == offenlegungslauf]
     ldf.drop(columns=["Offenlegungslauf", "Datenstand", "Datum", "Gewährungsdatum der Zuwendung"], inplace=True)
-    parties = list(set(ldf[PARTY_NAME]))
-    numparties = len(parties)
-    fig, axes = plt.subplots(int(np.ceil(np.sqrt(numparties))), int(np.ceil(np.sqrt(numparties))), figsize=(20, 20))
-    plt.rcParams.update({'font.size': 14})
+    return ldf
 
-    os.system(f"mkdir -p plots/{offenlegungslauf.replace(' ', '')}")
-    
-    for party in parties:
-        partydonors = ldf[ldf[PARTY_NAME] == party]
-        partydonors["Merged_Name"] = partydonors["Name des Urhebers der Zuwendung"].fillna('') + ' ' + partydonors[
-            "Vorname des Urhebers der Zuwendung"].fillna('') + ' ' + partydonors["Wohnsitzgemeinde"].fillna('')
-        partydonors["Merged"] = partydonors["Firma des Urhebers der Zuwendung"].fillna('') + ' ' + partydonors[
+
+def get_excel_data(filelocation):
+    df = pandas.read_excel(filelocation, sheet_name="Zuwendungen")
+    return df
+
+
+def plotperdonor(filelocation: str = "./2023_Parteifinanzierung.xlsx"):
+    global rowsdonor
+    df = get_excel_data(filelocation)
+    for offenlegungslauf in set(df["Offenlegungslauf"]):
+        ldf = extract_year_and_relevantdata(df, offenlegungslauf)
+        ldf["Merged_Name"] = ldf["Name des Urhebers der Zuwendung"].fillna('') + ' ' + ldf[
+            "Vorname des Urhebers der Zuwendung"].fillna('') + ' ' + ldf["Wohnsitzgemeinde"].fillna('')
+        ldf["Merged"] = ldf["Firma des Urhebers der Zuwendung"].fillna('') + ' ' + ldf[
             "Merged_Name"].str.strip()
-        partydonors["Merged"] = partydonors["Merged"].apply(lambda row: row.rstrip(" "))
-        partydonors["Merged"] = partydonors["Merged"].apply(lambda row: rightstrip_with_wildcards(row, "(*)"))
-        mpartydonors = renamesimilar(partydonors, "Merged")
-        a = partydonors.nlargest(NUMBEROFDONORSPERPLOT, "Wert (in CHF)")
-        if MULTIPLEPLOTSINONE:
-            ax = axes.flatten()[parties.index(party)]
-            ax.pie(a["Wert (in CHF)"], labels=a["Merged"])
-            ax.set_title(f"Zuwendungen {party} {offenlegungslauf}")
-        else:
-            plt.pie(a["Wert (in CHF)"], labels=a["Merged"])
-            title = f"Zuwendungen {party} {offenlegungslauf}"
-            if NUMBEROFDONORSPERPLOT < np.inf:
-                f"Zuwendungen {party} Top {NUMBEROFDONORSPERPLOT}"
-            plt.title(title)
-            # plt.show()
+        ldf["Merged"] = ldf["Merged"].apply(lambda row: row.rstrip(" "))
+        ldf["Merged"] = ldf["Merged"].apply(lambda row: rightstrip_with_wildcards(row, "(*)"))
+        ldf["Merged"] = ldf["Merged"].apply(lambda row: row.lstrip(" "))
+        ldf["Merged"] = ldf["Merged"].apply(lambda row: row.rstrip(" "))
+        ldf = renamesimilar(ldf, "Merged")
+        plotlocation = f"plots/{offenlegungslauf.replace(' ', '')}/DONORS"
+        os.system(f"mkdir -p {plotlocation}")
+        plt.rcParams.update({'font.size': 40})
+        for donor in set(ldf["Merged"]):
+            rowsdonor = ldf[ldf["Merged"] == donor]
+            numrecipents = len(rowsdonor)
+            sqrt_numrecpitents = int(np.ceil(np.sqrt(numrecipents)))
+            df = pandas.DataFrame(columns=[PARTY_NAME, "DONOR", COLNAME_AMOUNT])
+            for index, row in rowsdonor.iterrows():
+                party = row[PARTY_NAME]
+                merged = False
+                for p2 in df[PARTY_NAME]:
+                    if SequenceMatcher(None, party, p2).ratio() > 0.9:
+                        df[df[PARTY_NAME] == p2][COLNAME_AMOUNT] += row[COLNAME_AMOUNT]
+                        merged = True
+                        break
+                if not merged:
+                    df.loc[len(df)] = [party, "DONOR", row[COLNAME_AMOUNT]]
+            rowsdonor = df
+
+            plt.pie(rowsdonor["Wert (in CHF)"], labels=rowsdonor[PARTY_NAME],
+                    autopct=lambda p: '{:.0f}'.format(p * rowsdonor[COLNAME_AMOUNT].sum() / 100))
+            plt.title(f"Zuwendungen {donor}\n{offenlegungslauf}")
             plt.tight_layout()
-            plt.savefig(f"plots/{offenlegungslauf.replace(' ', '')}/{party}.png")
+            plt.savefig(f"{plotlocation}/{donor.replace('/', '')}.png")
             plt.clf()
-    if MULTIPLEPLOTSINONE:
-        for j in range(len(parties), len(axes.flatten())):
-            fig.delaxes(axes.flatten()[j])
+    return df
 
-        fig.tight_layout(rect=[0, 0, 1, 0.95])
-        suptitle = f"Zuwendungen für {offenlegungslauf}"
-        if NUMBEROFDONORSPERPLOT < np.inf:
-            suptitle += f" Top (n={NUMBEROFDONORSPERPLOT})"
-        plt.suptitle(suptitle)
-        plt.savefig(f"plots/{offenlegungslauf.replace(' ', '')}/all.png")
-
-    plt.clf()
-    ldf["Merged_Name"] = ldf["Name des Urhebers der Zuwendung"].fillna('') + ' ' + ldf[
-        "Vorname des Urhebers der Zuwendung"].fillna('') + ' ' + ldf["Wohnsitzgemeinde"].fillna('')
-    ldf["Merged"] = ldf["Firma des Urhebers der Zuwendung"].fillna('') + ' ' + ldf[
-        "Merged_Name"].str.strip()
-    ldf["Merged"] = ldf["Merged"].apply(lambda row: row.rstrip(" "))
-    ldf["Merged"] = ldf["Merged"].apply(lambda row: rightstrip_with_wildcards(row, "(*)"))
-    ldf["Merged"] = ldf["Merged"].apply(lambda row: row.lstrip(" "))
-    ldf["Merged"] = ldf["Merged"].apply(lambda row: row.rstrip(" "))
-    ldf = renamesimilar(ldf, "Merged")
-    plotlocation = f"plots/{offenlegungslauf.replace(' ', '')}/DONORS"
-    os.system(f"mkdir -p {plotlocation}")
-    plt.rcParams.update({'font.size': 40})
-
-    for donor in set(ldf["Merged"]):
-        rowsdonor = ldf[ldf["Merged"] == donor]
-        numrecipents = len(rowsdonor)
-        sqrt_numrecpitents = int(np.ceil(np.sqrt(numrecipents)))
-        df = pandas.DataFrame(columns=[PARTY_NAME, "DONOR" ,COLNAME_AMOUNT])
-        for index, row in rowsdonor.iterrows():
-            party = row[PARTY_NAME]
-            merged = False
-            for p2 in df[PARTY_NAME]:
-                if SequenceMatcher(None, party, p2).ratio() > 0.9:
-                    df[df[PARTY_NAME] == p2][COLNAME_AMOUNT] += row[COLNAME_AMOUNT]
-                    merged = True
-                    break
-            if not merged:
-                df.loc[len(df)] = [party, "DONOR", row[COLNAME_AMOUNT]]
-        rowsdonor = df
-        plt.pie(rowsdonor["Wert (in CHF)"], labels=rowsdonor[PARTY_NAME],
-                autopct=lambda p: '{:.0f}'.format(p * rowsdonor[COLNAME_AMOUNT].sum() / 100))
-        plt.title(f"Zuwendungen {donor}\n{offenlegungslauf}")
-        plt.tight_layout()
-        plt.savefig(f"{plotlocation}/{donor.replace('/', '')}.png")
-        plt.clf()
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Parse financial contributions and generate plots.')
+    parser.add_argument('--input_file', type=str, help='Path to the Excel file with the data', default="./2023_Parteifinanzierung.xlsx", required=False)
+    parser.add_argument('--num_donors', type=int, default=10, help='Number of top donors to display in each plot')
+    parser.add_argument("--add-rest", help="Show a reminder position in the plots", action="store_true")
+    parser.add_argument("--top-n", type=int, default=np.inf, help="Only show the top n donors in each plot.")
+    parser.add_argument("--multiple-plots-in-one", help="Show multiple plots in one figure", action="store_true")
+    args = parser.parse_args()
+    global NUMBEROFDONORSPERPLOT
+    NUMBEROFDONORSPERPLOT = args.num_donors
+
+    plotpartydonorsperparty(args.input_file, args.add_rest, args.multiple_plots_in_one)
+    print(
+        f"Plots are saved in the plots/ folder."
+    )
+    plotperdonor(args.input_file)
+
 if __name__ == '__main__':
     main()
